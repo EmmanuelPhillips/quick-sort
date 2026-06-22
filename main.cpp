6c6ec446aacb515031922a319958e243758bf7ba
RammusOTP <emmanuelphillips33@gmail.com>
2026-06-22 21:45:10 +0100

    wrote main in order to know waht code will look like

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
+
+void print_vector(std::vector<int> &v) {
+  for (auto &i : v) {
+    std::cout << i << ' ';
+  }
+  std::cout << '\n';
+}
+
+void quick_sort(std::vector<int> &v) {}
 
 int main() {
+  std::vector<int> v{8, 3, 12, 1, 7, 5, 10};
+  std::cout << "list before sorting: ";
+  print_vector(v);
+  quick_sort(v);
+  std::cout << "list after sotring: ";
+  print_vector(v);
 
-    return 0;
+  return 0;
 }
